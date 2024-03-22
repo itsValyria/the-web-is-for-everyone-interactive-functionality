b490929341f20246af3fc5135f9f6d1a91d87b6d
Valyria <76444716+itsValyria@users.noreply.github.com>
2024-03-22 10:16:25 +0100

    added service detail page

--- server.js
+++ server.js
@@ -1,90 +1,84 @@
 // Importeer het npm pakket express uit de node_modules map
-import express from 'express'
+import express from 'express';
 
 // Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
-import fetchJson from './helpers/fetch-json.js'
-
-// Fetch de data van de FDND Agency API
-const allData_advertisements = await fetchJson('https://fdnd-agency.directus.app/items/dh_services')
-let all_advertisements_data = allData_advertisements.data;
+import fetchJson from './helpers/fetch-json.js';
 
 // Maak een nieuwe express app aan
-const app = express()
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Stel ejs in als template engine
-app.set('view engine', 'ejs')
+// Fetch de data van de FDND Agency API
+const allData_advertisements = await fetchJson('https://fdnd-agency.directus.app/items/dh_services');
+let all_advertisements_data = allData_advertisements.data;
 
-// Stel de map met ejs templates in
-app.set('views', './views')
+// // Stel ejs in als template engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 // Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-// Maak een GET route voor de index
+// Counter voor het ID van de services
+let idCounter = all_advertisements_data.length;
+
+// GET route voor de index
 app.get('/', function (request, response) {
-  response.render('index', {services: all_advertisements_data})
-})
+  response.render('index', { services: all_advertisements_data });
+});
 
+// GET route voor ze overzichts pagina
 app.get('/overzicht', function (request, response) {
-  response.render('overzicht', {services: all_advertisements_data})
-})
+  response.render('overzicht', { services: all_advertisements_data });
+});
 
-app.get('/service-aanmelden', function (request, response) {
-  response.render('service-aanmelden', {services: all_advertisements_data})
-})
-
-app.get('/service-aanmelden-gelukt', function (request, response) {
-  response.render('service-aanmelden-gelukt', {services: all_advertisements_data})
-})
-
-// Maak een POST route voor de index
-app.post('/', function (request, response) {
-  // Er is nog geen afhandeling van POST, redirect naar GET op /
-  response.redirect(303, '/')
-})
-
-// Maak een GET route voor een detailpagina met een request parameter id
+// Maak een GET route voor een detail pagina
 app.get('/service/:id', function (request, response) {
-  // Gebruik de request parameter id en haal de juiste persoon uit de WHOIS API op
-  fetchJson('https://fdnd-agency.directus.app/items/dh_services/' + request.params.id).then((apiData) => {
-    // Render service.ejs uit de views map en geef de opgehaalde data mee als variable, genaamd service
-    response.render('service', {service: apiData.data})
-  })
-})
+  response.render('service', { services: all_advertisements_data });
+});
+
+// GET route for the service aanmelden pagina
+app.get('/service-aanmelden', function (request, response) {
+  response.render('service-aanmelden', { services: all_advertisements_data });
+});
+
+// GET route voor de service aanmelden gelukt pagina
+app.get('/service-aanmelden-gelukt', function (request, response) {
+  response.render('service-aanmelden-gelukt', { services: all_advertisements_data });
+});
 
 // POST route om data van het formulier te handlen
 app.post('/service-aanmelden', function (request, response) {
   const formData = request.body;
 
-  // Maak een nieuw object met formData
-  const newAdvertisement = {
-    name: formData.name,
-    surname: formData.surname,
-    email: formData.email,
-    contact: formData.contact,
-    title: formData.title,
-    short_description: formData.short_description,
-    long_description: formData.long_description,
-    location: formData.location,
-    neighbourhood: formData.neighbourhood,
-    start_date: formData.start_date,
-    end_date: formData.end_date,
-    start_time: formData.start_time,
-    end_time: formData.end_time,
-    image: formData.image
-  };
+// Maak een nieuw object met formData
+const newAdvertisement = {
+  id: ++idCounter, // Verhoog de ID count
+  name: formData.name,
+  surname: formData.surname,
+  email: formData.email,
+  contact: formData.contact,
+  title: formData.title,
+  short_description: formData.short_description,
+  long_description: formData.long_description,
+  location: formData.location,
+  neighbourhood: formData.neighbourhood,
+  start_date: formData.start_date,
+  end_date: formData.end_date,
+  start_time: formData.start_time,
+  end_time: formData.end_time,
+};
 
-  // Push het nieuwe object naar het all_advertisements_data array
-  all_advertisements_data.push(newAdvertisement);
+// Push het nieuwe object naar het all_advertisements_data array
+all_advertisements_data.push(newAdvertisement);
 
-  // Redirect naar de gelukt pagina
-  response.redirect('/service-aanmelden-gelukt');
+// Redirect naar de gelukt pagina
+response.redirect('/service-aanmelden-gelukt');
 });
 
 // Stel het poortnummer in waar express op moet gaan luisteren
-app.set('port', process.env.PORT || 8000)
+app.set('port', process.env.PORT || 8000);
 
 // Start express op, haal daarbij het zojuist ingestelde poortnummer op
 app.listen(app.get('port'), function () {
